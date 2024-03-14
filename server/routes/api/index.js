35057dcfe8e02254ec79a0debaad40620c8f6b2c
Flesh <olivia.a.gowan@gmail.com>
2024-03-13 20:42:13 -0400

    comment routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogpostRoutes = require('./blogpostRoutes');
+const commentRoutes = require('./commentRoutes');
 
+router.use('/comments', commentRoutes);
 router.use('/blogposts', blogpostRoutes);
 router.use('/users', userRoutes);
 
