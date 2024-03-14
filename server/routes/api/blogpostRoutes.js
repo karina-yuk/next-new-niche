79802dc268cc0b95a5035b5b91facc826261b97c
Flesh <olivia.a.gowan@gmail.com>
2024-03-13 20:38:59 -0400

    blogpost routes

---
+++ server/routes/api/blogpostRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getBlogposts,
+  getSingleBlogpost,
+  createBlogpost,
+  updateBlogpost,
+  deleteBlogpost
+} = require('../../controllers/blogpostController.js');
+
+// /api/blogposts
+router.route('/').get(getBlogposts).post(createBlogpost);
+
+// /api/blogposts/:blogpostId
+router
+  .route('/:blogpostId')
+  .get(getSingleBlogpost)
+  .put(updateBlogpost)
+  .delete(deleteBlogpost);
+
+module.exports = router;
