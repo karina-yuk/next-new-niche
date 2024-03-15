fc6271ee644ca1ebfc09aef818add063bb3fa894
Flesh <olivia.a.gowan@gmail.com>
2024-03-14 20:12:58 -0400

    added user controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,77 @@
-const { User } = require('../models');
+const { User, Blogpost } = require('../models');
 
 module.exports = {
-  async getUsers(req, res) {},
-  async getSingleUser(req, res) {},
-  async createUser(req, res) {},
-  async updateUser(req, res) {},
-  async deleteUser(req, res) {},
-  async addFriend(req, res) {},
-  async removeFriend(req, res) {},
+  // get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // get single user
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findByPk({ _id: req.params.id })
+      .populate('blogposts');
+
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // create a user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json({ message: 'New user created', user });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // update a user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate({ _id: req.params.userId }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json({ message: 'User updated', user});
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // delete a user and their blogposts
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'student not found' });
+      }
+
+      const blogposts = await Blogpost.deleteMany({ _id: { $in: user.blogposts } });
+
+      if (!blogposts) {
+        return res.status(404).json({ message: 'user deleted and no blogposts found' });
+      }
+
+      res.json({ message: 'User and blogposts deleted' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
