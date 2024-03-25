c7c203964ebbbd9bb7e67ba8136f20c2218a12c9
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-03-24 20:05:09 -0400

    Added content on Events component

--- client/src/components/Events/index.jsx
+++ client/src/components/Events/index.jsx
@@ -6,17 +6,17 @@ const Events = ({ Header, darkTheme }) => {
   const wrapperRef = useRef(null); // Reference to the wrapper for detecting outside
   const events = [
     {
-      imageUrl: "https://e22u.short.gy/KTU286",
-      // imageUrl: "https://example.com/path/to/your/image.jpg",
+      imageUrl: "https://i.pinimg.com/564x/45/6e/10/456e100b6448a5b773a0363d1b2c8d55.jpg",
+      
       url: "https://www.google.com",
-      title: "Event Title 1",
-      description: "This is a small description for Event 1. It should not exceed 30 words, but should be informative enough to give context."
+      title: "",
+      description: ""
     },
     {
-      imageUrl: "https://e22u.short.gy/KTU286",
+      imageUrl: "https://lh3.googleusercontent.com/-huSeQKd_-p8/W5DrHdvLOBI/AAAAAAABYQA/RP1o15IgfdsF67agyFnqrRBOYt_A_WaSwCHMYCw/cricova-winery-913?imgmax=1600",
       url: "https://e22u.short.gy/KTU286",
-      title: "Event Title 1",
-      description: "This is a small description for Event 1. It should not exceed 30 words, but should be informative enough to give context."
+      title: "",
+      description: ""
     },
     {
       imageUrl: "https://facts.net/wp-content/uploads/2023/07/15-facts-about-charleston-wine-food-festival-1690069801.jpg",
@@ -31,7 +31,7 @@ const Events = ({ Header, darkTheme }) => {
       description: "The food and wine classic which is a two days festival, offering food and wine tasting and many more fun activities returns in 2023! The event will be held in Walt Disney World Swan and Dolphin, which is located in the heart of Walt Disney World Resort. This resort offers a wide range of dining experiences, from quick service to fine dining, and an extensive list of wines and beers that are available by the glass or bottle. Guests can enjoy these beverages and food at any time throughout the festival. The event also has a variety of options for guests who are looking for something more than just food and drink. The resort offers a number of recreational activities including swimming pools, spas, golf courses, moreover if you stay overnight. And of course, live music and other performances will take place during the entire festival."
     },
     {
-      imageUrl: "https://e22u.short.gy/KTU286",
+      imageUrl: "https://i.pinimg.com/564x/6f/e0/99/6fe0994de4b59b7e0bcec893f211aaa9.jpg",
       url: "https://www.carnifest.com/its-beaujolais-nouveau-time-festival-2024/",
       title: "Beaujolais Nouveau Time Festival",
       description: "The Beaujolais Nouveau Time Festival is a celebration of the release of new Beaujolais wine. It takes place every year on the third Wednesday in November in this region of south Burgundy. The guests are supposed to taste and compare 12 varieties of Beaujolais wine. Beaujolais Nouveau is a type of red wine made in the Burgundy region of France around the town of Beaujeu and released every autumn in November. The festival typically lasts a few days and includes various activities such as concerts, street markets, fireworks displays, walks, games, film screenings, etc."
@@ -43,34 +43,34 @@ const Events = ({ Header, darkTheme }) => {
       description: "One of the most interesting events among Tuscany wine festivals, he Bravìo delle Botti is the challenging barrel race between eight districts: Cagnano, Collazzi, Le Coste, Gracciano, Poggiolo, San Donato, Talosa and Voltaia, all located along the historic city center. They compete for a painted cloth by rolling barrels of about 80 kg each, along an uphill path of over a kilometer. The barrels are pushed by two athletic pushers, while the route of the race winds through the picturesque streets of the historic center of the city of Poliziana until the arrival, located in the churchyard of the Duomo in Piazza Grande. It’s one of the most spectacular wine events in Tuscany."
     },
     {
-      imageUrl: "https://e22u.short.gy/KTU286",
-      url: "https://e22u.short.gy/KTU286",
-      title: "Event Title 1",
-      description: "This is a small description for Event 1. It should not exceed 30 words, but should be informative enough to give context."
+      imageUrl: "https://moldovatovisit.eu/wp-content/uploads/2018/04/moldova-wineries-tour-4-1024x658.jpg",
+      url: "https://moldova.travel/en/rutele-vietii/ziua-nationala-a-vinului/",
+      title: "Moldova's Wine Festival",
+      description: "Being an annual tradition and appreciated by the amateurs of good wines, both in the country and abroad, the Celebration of the Wine of Moldova is held every year on the first weekend of October in the Square of the Great"
     },
     {
-      imageUrl: "https://e22u.short.gy/KTU286",
-      url: "https://e22u.short.gy/KTU286",
-      title: "Event Title 1",
-      description: "This is a small description for Event 1. It should not exceed 30 words, but should be informative enough to give context."
+      imageUrl: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/420046892_768184115346173_77036514565723437_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=K7UvmG8bUaUAX_ix0mK&_nc_ht=scontent-ord5-2.xx&oh=00_AfDqE2Dy_tMfBtMQl0bhZKxjgZvp-ZzgJLgoMlkG871c9w&oe=66066589",
+      url: "https://niagarawinefestival.com/",
+      title: "SPRING SPARKLES FESTIVAL",
+      description: "Let’s add a bit of sparkle to the early days of spring! Wake up from winter and kick off spring with fizz and fun. The Spring Sparkles Festival celebrates all 100% Ontario VQA Sparkling wines – traditional or charmat method, of any varietal or style. Integrate this wine into your everyday life and add some sparkle to your day, even in the humdrum of a long, Canadian winter."
     },
     {
-      imageUrl: "https://e22u.short.gy/KTU286",
-      url: "https://e22u.short.gy/KTU286",
-      title: "Event Title 1",
-      description: "This is a small description for Event 1. It should not exceed 30 words, but should be informative enough to give context."
+      imageUrl: "https://www.spain.info/export/sites/segtur/.content/imagenes/reportajes/rioja-la/fiesta-vendimia-la-rioja.jpg",
+      url: "https://www.spain.info/en/discover-spain/fiestas-grape-harvest-rioja/",
+      title: "The San Mateo Festival ",
+      description: "The San Mateo Festival is an event that happens every year in the Rioja wine region in Spain. It is a celebration of the grape harvest, and it attracts people from all over the world to come and enjoy a week of festivities. This festival has been happening since 1956 first known as Fiestas de la Vendimia (Harvest Parties or Festivals). Nowadays, it's one of the most popular events of its kind which includes many activities such as grape harvesting, massive wine and food tasting, dancing, etc. Also, one of the emphasis of the event is squeezing the must out of the grapes in the conventional way by pressing the grapes with the people feet. Therefore, with the help of a local wine producer, one can get an idea of what goes into the process. The Rioja Wine Harvest Festivity is one of the most important events in the calendar for wine producers and their families, as well as for all those who love wine and are interested in knowing more about it."
     },
     {
-      imageUrl: "https://e22u.short.gy/KTU286",
-      url: "https://e22u.short.gy/KTU286",
-      title: "Event Title 1",
-      description: "This is a small description for Event 1. It should not exceed 30 words, but should be informative enough to give context."
+      imageUrl: "https://i.pinimg.com/564x/3c/77/da/3c77da9a3e11cf13a0f82d77ac00d9d0.jpg",
+      url: "",
+      title: "",
+      description: ""
     },
     {
-      imageUrl: "https://e22u.short.gy/KTU286",
-      url: "https://e22u.short.gy/KTU286",
-      title: "Event Title 1",
-      description: "This is a small description for Event 1. It should not exceed 30 words, but should be informative enough to give context."
+      imageUrl: "https://i.pinimg.com/564x/02/51/5d/02515dcf6a7122f956db8c82bc19babf.jpg",
+      url: "",
+      title: "",
+      description: ""
     },
   ];
 
