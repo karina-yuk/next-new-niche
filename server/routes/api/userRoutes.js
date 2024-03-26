ba3a7555d4a88524345e05d10a4b28cc214ed82c
harry <harrymac1972@gmail.com>
2024-03-26 07:16:03 -0600

    routes/api/blogpost & comment & user formatting cleaned up

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -8,7 +8,10 @@ const {
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router
+  .route('/')
+  .get(getUsers)
+  .post(createUser);
 
 // /api/users/:userId
 router
