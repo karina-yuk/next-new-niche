df55fc83ee825103833de65d9e3058b389cd5bfe
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-03-26 23:55:16 -0400

    Added social links in subscribe component

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -192,7 +192,7 @@ const Header = ({ isLoggedIn, handleNavClick }) => {
 
       <div className="social-links">
         <a
-          href="https://linkedin.com/in/stanislavmorozan"
+          href="https://linkedin.com"
           target="_blank"
           rel="noopener noreferrer"
           className="icon-link"
@@ -200,7 +200,7 @@ const Header = ({ isLoggedIn, handleNavClick }) => {
           <i className="fab fa-linkedin-in"></i>
         </a>
         <a
-          href="https://github.com/Stas-Cell-Max"
+          href="https://github.com"
           target="_blank"
           rel="noopener noreferrer"
           className="icon-link"
@@ -208,7 +208,7 @@ const Header = ({ isLoggedIn, handleNavClick }) => {
           <i className="fab fa-github"></i>
         </a>
         <a
-          href="https://facebook.com/stas.moozan"
+          href="https://facebook.com"
           target="_blank"
           rel="noopener noreferrer"
           className="icon-link"
@@ -216,7 +216,7 @@ const Header = ({ isLoggedIn, handleNavClick }) => {
           <i className="fab fa-facebook-f"></i>
         </a>
         <a
-          href="https://twitter.com/yourusername"
+          href="https://twitter.com"
           target="_blank"
           rel="noopener noreferrer"
           className="icon-link"
@@ -224,7 +224,7 @@ const Header = ({ isLoggedIn, handleNavClick }) => {
           <i className="fab fa-twitter"></i>
         </a>
         <a
-          href="https://instagram.com/stas_moozan"
+          href="https://instagram.com"
           target="_blank"
           rel="noopener noreferrer"
           className="icon-link"
